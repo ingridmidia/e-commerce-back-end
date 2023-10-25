4501312f65973f621ea19dbcbc03ee923637d379
ingridmidia <ingridmidia@gmail.com>
2023-10-24 20:57:32 -0500

    implemented post route /api/categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,8 +13,21 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  /* req.body should look like this...
+    {
+      "category_name": "Socks"
+    }
+  */
+  try {
+    if (req.body.category_name) {
+      const newCategory = await Category.create(req.body);
+      res.status(200).json(newCategory);
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
