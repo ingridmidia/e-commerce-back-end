a02ecfa082cbdc3986dc42141d1e65682d94eb77
ingridmidia <ingridmidia@gmail.com>
2023-10-25 16:19:37 -0500

    implemented put /api/categories/:id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,8 +49,21 @@ router.post('/', async (req, res) => {
 });
 
 // update a category by its `id` value
-router.put('/:id', (req, res) => {
-
+router.put('/:id', async (req, res) => {
+  try {
+    const category = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+    if (category[0] === 0) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // delete a category by its `id` value
