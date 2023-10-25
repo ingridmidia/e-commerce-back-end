7ae9032b40d41f3c3615dca34a9f435f0aa6fc69
ingridmidia <ingridmidia@gmail.com>
2023-10-24 21:13:00 -0500

    implemented delete route /api/categories/:id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,8 +34,22 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedCategory) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
