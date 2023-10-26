c729877d6824de168470c7a40fa91b6f9223127d
ingridmidia <ingridmidia@gmail.com>
2023-10-26 16:22:00 -0500

    implemented post /api/tags/

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,8 +31,21 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+router.post('/', async (req, res) => {
+  /* req.body should look like this...
+    {
+      "tag_name": "wooden bats"
+    }
+  */
+  try {
+    if (req.body.tag_name) {
+      const newTag = await Tag.create(req.body);
+      res.status(200).json(newTag);
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
