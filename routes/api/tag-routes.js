cc071ef46654cd2346e9aebe085df890f7c53f34
ingridmidia <ingridmidia@gmail.com>
2023-10-26 16:19:15 -0500

    implemented get /api/tags/:id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,9 +15,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// find a single tag by its `id`
+router.get('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tag) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
