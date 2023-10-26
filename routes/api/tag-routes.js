5aea754fb5c4312b84017b8b1575e7da255f9dea
ingridmidia <ingridmidia@gmail.com>
2023-10-26 16:27:29 -0500

    implemented delete /api/tags/:id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -66,8 +66,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletedTag) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag deleted.' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
